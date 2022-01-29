8db0807db7f61d46b626c7ca98237fe2c0eff915
bruno.muehlbauer <bruno.muehlbauer@contaazul.com>
2022-01-29 12:47:48 -0300

    addded swagger

--- src/main/java/com/contaazul/controllers/InvoiceIssuerController.java
+++ src/main/java/com/contaazul/controllers/InvoiceIssuerController.java
@@ -4,8 +4,10 @@ import static java.lang.String.format;
 
 import lombok.AllArgsConstructor;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.contaazul.entities.Invoice;
